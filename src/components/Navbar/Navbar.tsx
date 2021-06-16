8cdc6fa4528ffb7de4cf8dbe22db4cd744b8b76b
Naytin <worlddesign1987@gmail.com>
2021-06-16 23:28:42 +0300

    add icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import style from './Navbar.module.scss'
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 export const Navbar = () => {
+    const location = useLocation()
+    console.log(location)
     return (
         <nav className={style.nav}>
             <ul className={style.nav__list}>
-                <Link className={`${style.nav__item} icon-profile`} to='/profile'/>
-                <Link className={`${style.nav__item} icon-commenting-o`} to='/dialogs'/>
+                <Link className={`${style.nav__item} icon-profile ${location.pathname === '/profile' ? style.active : null}`} to='/profile'/>
+                <Link className={`${style.nav__item} icon-commenting-o ${location.pathname === '/dialogs' ? style.active : null}`} to='/dialogs'/>
                 {/*<Link className={style.nav__item} to='/news'> News</Link>*/}
                 {/*<Link className={style.nav__item} to='/music'>Music</Link>*/}
                 {/*<Link className={style.nav__item} to='/settings'>Settings</Link>*/}
-                <Link className={`${style.nav__item} icon-group`} to='/users'/>
+                <Link className={`${style.nav__item} icon-group ${location.pathname === '/users' ? style.active : null}`} to='/users'/>
             </ul>
         </nav>
     )
